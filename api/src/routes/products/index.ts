4e4557e89e6bd8d9207bb89872fed4316cf12cab
Lewis <lewis@MacBook-Pro-Lewis.local>
2024-11-04 13:05:57 +0300

    product router and controller

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,19 +1,20 @@
 import { Router } from "express";
 
+import {
+    createProduct,
+    deleteProduct,
+    getProductById,
+    listProducts,
+    updateProduct
+} from "./productsController";
+
 // Products endpoints
 const router = Router();
 
-router.get('/', (req, res) => {
-    console.log(req.params)
-    res.send('This is a list of products')
-})
-router.post('/', (req, res) => {
-    res.send('Create a products!')
-})
-
-router.get('/:id', (req, res) => {
-    console.log(req.params)
-    res.send('Product Id: 123')
-})
+router.get('/', listProducts)
+router.post('/', createProduct)
+router.get('/:id', getProductById)
+router.put('/:id', updateProduct)
+router.delete('/:id', deleteProduct)
 
 export default router
